96270c4b1c015f8fb96d379086e5338291f82a74
javaswapdevelopment <javaswap.development@gmail.com>
2021-12-31 20:40:19 -0500

    Change the background for overolay

--- packages/java-uikit/src/components/Overlay/Overlay.tsx
+++ packages/java-uikit/src/components/Overlay/Overlay.tsx
@@ -8,7 +8,7 @@ const StyledOverlay = styled(Box)`
   left: 0px;
   width: 100%;
   height: 100%;
-  background-color: ${({ theme }) => `${theme.colors.text}99`};
+  background-color: transparent;
   backdrop-filter: blur(2px);
   z-index: 20;
 `;
